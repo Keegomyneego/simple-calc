5189ab8f9734e9317340ea8c5ecab086fb646a17
Keegan Farley <keegomyneego@live.com>
2016-10-05 03:00:38 -0700

    got basic in out flow worked out

--- simple-calc/main.swift
+++ simple-calc/main.swift
@@ -8,5 +8,52 @@
 
 import Foundation
 
-print("Hello, World!")
+typealias OperatorFunction = ([UInt]) -> UInt
+
+class SimpleOperation {
+
+    let minOperands: Int
+    let operate: OperatorFunction
+
+    init(minOperands: Int, operation: @escaping OperatorFunction) {
+        self.minOperands = minOperands
+        self.operate = operation
+    }
+}
+
+var numericalInputHistory: [UInt] = []
+
+var operatorFunctions: [String : SimpleOperation] = [
+    "+" : SimpleOperation(minOperands: 2, operation: { inputs in
+        return inputs.reduce(0, +)
+    })
+]
+
+func promptForInput() -> UInt {
+    repeat {
+        let input = readLine(strippingNewline: true)!
+
+        if let numericalInput = UInt.init(input) {
+            numericalInputHistory.append(numericalInput)
+        } else if let operatorFunction = operatorFunctions[input] {
+            return operatorFunction.operate(numericalInputHistory)
+        } else {
+            print("\"\(input)\" command not recognized. Try one of \(operatorFunctions.keys)")
+        }
+    } while true
+}
+
+
+// Main logic
+repeat {
+    print("Enter an expression separated by returns:")
+
+    let result = promptForInput()
+
+    print("Result: \(result)")
+    print("")
+    print("")
+} while true
+
+
 
